86c6abeacaab36508891f9d5a4029cae63126b02
Kxoe <kxoethecat@gmail.com>
2018-06-26 01:38:25 -0600

    Added isometric projection calculations and display for such calculations

---
+++ IsometricSquare.java
@@ -0,0 +1,32 @@
+
+
+class IsometricSquare {
+
+    double angleFacing; ///The angle at which the observer is observing
+    int diagonalLength; ///The length of the diagonal of the square
+    int centerX; ///The x-coordinate of the isometric square
+    int centerY; ///the y-coordinate of the isometric square
+
+    public IsometricSquare(double angle, int diagonalLength, int centerX, int centerY) {
+        this.angleFacing = angle;
+        this.diagonalLength = diagonalLength;
+        this.centerX = centerX;
+        this.centerY = centerY;
+    }
+
+    public int[][] getVertices() {
+        int[] verticesX = new int[4];
+        int[] verticesY = new int[4];
+        verticesX[0] = centerX - (int) (Math.sin(angleFacing) * (double) diagonalLength);
+        verticesX[1] = centerX;
+        verticesX[2] = centerX + (int) (Math.sin(angleFacing) * (double) diagonalLength);
+        verticesX[3] = centerX;
+        verticesY[0] = centerY;
+        verticesY[1] = centerY - diagonalLength;
+        verticesY[2] = centerY;
+        verticesY[3] = centerY + diagonalLength;
+        int[][] allVertices = {verticesX, verticesY};
+        return allVertices;
+    }
+
+}
--- IsometricityWindow.java
+++ IsometricityWindow.java
@@ -6,6 +6,8 @@ import javax.swing.*;
  */
 class IsometricityWindow extends JFrame {
 
+    public IsometricSquare square; ///An isometric square object to be drawn
+
     /**
      * Constructs a new window with the given title
      */
@@ -14,26 +16,34 @@ class IsometricityWindow extends JFrame {
         this.setVisible(true);
         this.setSize(500, 400);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.square = new IsometricSquare(0.52, 25, 75, 75); 
     } 
 
     /**
      * Draws all pieces of the window
-     * Draws a square rotated 45 degrees
+     * Draws an isometrically shifted square
      */
     public void paint(Graphics g) {
         g.setColor(Color.LIGHT_GRAY);
         g.fillRect(0, 0, 500, 400);
         g.setColor(Color.BLACK);
-        int[] xPoints = {75, 100, 75, 50};
-        int[] yPoints = {50, 75, 100, 75};
-        g.fillPolygon(xPoints, yPoints, 4);
+        int[][] vertices = square.getVertices();
+        g.fillPolygon(vertices[1], vertices[0], 4);
     }
 
     /**
      * Main method; initializes a window
      */
     public static void main(String[] args) {
-        JFrame display = new IsometricityWindow("Isometricity Tester");
-    }
+        IsometricityWindow display = new IsometricityWindow("Isometricity Tester");
+        while(true) {
+            try {
+                Thread.sleep(17);
+            } catch (Exception e) {
 
+            }
+            display.square.angleFacing += 0.01;
+            display.repaint();
+        }
+    }
 }
